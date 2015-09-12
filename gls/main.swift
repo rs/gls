030c6aba2e8cfdb47d176376ef8c83ce6392e67c
Olivier Poitrey <rs@dailymotion.com>
2015-09-12 01:33:16 -0700

    Add support for list serveral paths and for a and F flags

--- gls/main.swift
+++ gls/main.swift
@@ -9,77 +9,169 @@
 import Foundation
 import AppKit
 
+
+var opt_showInvisibles = false
+var opt_suffix = false
+
+let options = "aF"
+var buffer = Array(options.utf8).map { Int8($0) }
+
+while  true {
+    let option = Int(getopt(Process.argc, Process.unsafeArgv, buffer))
+    if option == -1 {
+        break
+    }
+    switch "\(UnicodeScalar(option))"
+    {
+    case "a":
+        opt_showInvisibles = true
+    case "F":
+        opt_suffix = true
+    case "?":
+        let charOption = "\(UnicodeScalar(Int(optopt)))"
+        print("usage: ls [-\(options)] [file ...]")
+        exit(1)
+    default:
+        abort()
+    }
+}
+
+var args : [String] = []
+for index in optind..<Process.argc {
+    args.append(String.fromCString(Process.unsafeArgv[Int(index)])!)
+}
+
+let graphicOutput = isatty(STDOUT_FILENO) != 0
 let fileManager = NSFileManager.defaultManager()
 let workspace = NSWorkspace.sharedWorkspace()
 
-// Read path from first argument if any, fallback to current directory
-var path = fileManager.currentDirectoryPath
-if Process.arguments.count >= 2 {
-    path = Process.arguments[1]
-    if Array(arrayLiteral: path)[0] != "/" {
-        // Use NSString as Xcode 7 deosn't bridge path related methods on String
-        let cwd = fileManager.currentDirectoryPath as NSString
-        path = cwd.stringByAppendingPathComponent(path)
+func listDirectory(path : NSURL) {
+    // List directory content
+    var items : [AnyObject]?
+
+    let properties = [NSURLFileResourceTypeKey, NSURLIsExecutableKey]
+    var options = NSDirectoryEnumerationOptions()
+    if !opt_showInvisibles {
+        options = .SkipsHiddenFiles
     }
-}
-
-// List directory content
-var contents : [AnyObject]?
-let graphicOutput = isatty(STDOUT_FILENO) != 0
-let pathURL = NSURL(fileURLWithPath: path)
-let properties = [NSURLIsSymbolicLinkKey, NSURLFileResourceTypeKey]
-do {
-    contents = try fileManager.contentsOfDirectoryAtURL(pathURL, includingPropertiesForKeys: properties, options: (.SkipsHiddenFiles))
-} catch let error as NSError {
-    print(error.localizedDescription, terminator: "\n")
-    exit(1)
-}
-
-for url in contents as! [NSURL] {
-    path = url.path!
-
-    if graphicOutput {
-        // Get file icon
-        let icon = workspace.iconForFile(path)
-
-        // Resize
-        let scaledSize = NSSize(width: 16, height: 16)
-        let scaledIcon = NSImage(size: scaledSize)
-        scaledIcon.lockFocus()
-        icon.drawInRect(NSRect(x: 0, y: 0, width: scaledSize.width, height: scaledSize.height), fromRect: CGRect(origin: CGPointZero, size: icon.size), operation: NSCompositingOperation.CompositeSourceOver, fraction: CGFloat(1.0))
-        scaledIcon.unlockFocus()
-
-        // Convert to base64
-        var data: NSData = scaledIcon.TIFFRepresentation!
-        var bitmap: NSBitmapImageRep = NSBitmapImageRep(data: data)!
-        data = bitmap.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])!
-        let iconB64 = data.base64EncodedStringWithOptions(NSDataBase64EncodingOptions())
-
-        // Output line
-        print("\u{001B}]1337;File=inline=1;height=1;width=2;preserveAspectRatio=true:", terminator: "")
-        print(iconB64, terminator: "")
-        print("\u{0007}", terminator: "")
-    }
-    print(url.lastPathComponent!, terminator: "")
-
     do {
+        items = try fileManager.contentsOfDirectoryAtURL(path, includingPropertiesForKeys: properties, options: options)
+    } catch let error as NSError {
+        print(error.localizedDescription)
+        exit(1)
+    }
+    showItems(items as! [NSURL])
+}
+
+func showItems(items : [NSURL]) {
+    for url in items {
+        let path = url.path!
+
+        var type : String
+        var exec : Bool
+        do {
+            // Fetch file info
+            var rsrc: AnyObject?
+            try url.getResourceValue(&rsrc, forKey: NSURLFileResourceTypeKey)
+            type = (rsrc as? String)!
+            try url.getResourceValue(&rsrc, forKey: NSURLIsExecutableKey)
+            exec = (rsrc as? Bool)!
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            exit(1)
+        }
+
+        if graphicOutput {
+            // Get file icon
+            let icon = workspace.iconForFile(path)
+
+            // Resize
+            let scaledSize = NSSize(width: 16, height: 16)
+            let scaledIcon = NSImage(size: scaledSize)
+            scaledIcon.lockFocus()
+            icon.drawInRect(NSRect(x: 0, y: 0, width: scaledSize.width, height: scaledSize.height), fromRect: CGRect(origin: CGPointZero, size: icon.size), operation: NSCompositingOperation.CompositeSourceOver, fraction: CGFloat(1.0))
+            scaledIcon.unlockFocus()
+
+            // Convert to base64
+            var data: NSData = scaledIcon.TIFFRepresentation!
+            let bitmap: NSBitmapImageRep = NSBitmapImageRep(data: data)!
+            data = bitmap.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])!
+            let iconB64 = data.base64EncodedStringWithOptions(NSDataBase64EncodingOptions())
+
+            // Output line
+            print("\u{001B}]1337;File=inline=1;height=1;width=2;preserveAspectRatio=true:", terminator: "")
+            print(iconB64, terminator: "")
+            print("\u{0007}", terminator: "")
+        }
+        print(url.lastPathComponent!, terminator: "")
+
         // Add file type specific info
-        var rsrc: AnyObject?
-        try url.getResourceValue(&rsrc, forKey: NSURLFileResourceTypeKey)
-        if let type = rsrc as? String {
-            switch type {
-            case NSURLFileResourceTypeDirectory:
+        switch type {
+        case NSURLFileResourceTypeDirectory:
+            if opt_suffix {
                 print("/", terminator: "")
-            case NSURLFileResourceTypeSymbolicLink:
-                do {
-                    var dest = try fileManager.destinationOfSymbolicLinkAtPath(path)
-                    print(" -> ", terminator: "")
-                    print(dest, terminator: "")
-                }
-            default:
-                break
             }
+        case NSURLFileResourceTypeSymbolicLink:
+            if opt_suffix {
+                print("@", terminator: "")
+            }
+            do {
+                let dest = try fileManager.destinationOfSymbolicLinkAtPath(path)
+                print(" -> ", terminator: "")
+                print(dest, terminator: "")
+            } catch {
+            }
+        case NSURLFileResourceTypeSocket:
+            if opt_suffix {
+                print("=", terminator: "")
+            }
+        case NSURLFileResourceTypeSocket:
+            if opt_suffix {
+                print("|", terminator: "")
+            }
+        default:
+            break
+        }
+        if exec && type != NSURLFileResourceTypeDirectory && opt_suffix {
+            print("*", terminator: "")
+        }
+        print("") // Prints the CR
+    }
+}
+
+// List given paths or current directory if no paths provided
+if args.count >= 1 {
+    var first = true
+    var files : [NSURL] = []
+    var dirs : [NSURL] = []
+    for path in args {
+        do {
+            var url = NSURL(fileURLWithPath: path)
+            var rsrc: AnyObject?
+            try url.getResourceValue(&rsrc, forKey: NSURLFileResourceTypeKey)
+            if rsrc as? String == NSURLFileResourceTypeDirectory {
+                dirs.append(url)
+            } else {
+                files.append(url)
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            exit(1)
         }
     }
-    print("") // Prints the CR
+    if files.count > 0 {
+        first = false
+        showItems(files)
+    }
+    for dir in dirs {
+        if !first {
+            print("")
+        }
+        first = false
+        print(dir.relativePath!, ":", separator: "")
+        listDirectory(dir)
+    }
+} else {
+    listDirectory(NSURL(fileURLWithPath: "."))
 }
+
